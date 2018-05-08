d1fa793b998b5ef113fec53afdb9ce23f04e92f8
dmickelson <davidpmickelson@gmail.com>
2018-05-08 09:33:08 -0400

    Finished v1

--- js/main.js
+++ js/main.js
@@ -15,6 +15,26 @@ const intvl = setInterval(() => {
 
   // time calculations days, hrs, minutes
   const days = Math.floor(distance/ (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 *60));
-  const mins = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000*60 * 60));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display result
+  // Using ES6/ES2015 Template String, these are NOT single quotes but back marks
+  // Users variables with template strings
+  countdown.innerHTML = `
+    <div>${days}<span> Days</span></div>
+    <div>${hours}<span>Hours</span></div>
+    <div>${mins}<span>Minutes</span></div>
+    <div>${seconds}<span>Seconds</span></div>
+  `;
+
+  // Has the time/launch date already past?
+  if (distance <0 ) {
+    // stop countdown
+    clearInterval(intvl);
+    // style and output text
+    countdown.style.color = "#17a2b8";
+    countdown.innerHTML = "Launched!";
+  }
 }, 1000);
